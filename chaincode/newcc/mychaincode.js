50a41e894e375cb14df0565ab9e443fe544efa17
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-02-08 17:39:41 +0530

    Update mychaincode.js

--- chaincode/newcc/mychaincode.js
+++ chaincode/newcc/mychaincode.js
@@ -1,66 +1,33 @@
 'use strict';
-const shim = require('fabric-shim');
-const util = require('util');
+const { Contract} = require('fabric-contract-api');
 
-let Chaincode = class {
-
-    async Init(stub) {
-    console.info('=========== Instantiated Private chaincode ===========');
-    return shim.success();
-  }
-
-  
-  async Invoke(stub) {
-    let ret = stub.getFunctionAndParameters();
-    console.info(ret);
-
-    let method = this[ret.fcn];
-    if (!method) {
-      console.error('no function of name:' + ret.fcn + ' found');
-      throw new Error('Received unknown function ' + ret.fcn + ' invocation');
-    }
-    try {
-      let payload = await method(stub, ret.params);
-      return shim.success(payload);
-    } catch (err) {
-      console.log(err);
-      return shim.error(err);
-    }
-  }
-
-  async queryProduct(stub, args) {
-    if (args.length != 2) {
-      throw new Error('Incorrect number of arguments. Expecting CarNumber ex: CAR01');
-    }
-    
-
-    let productAsBytes = await stub.getPrivateData(args[0],args[1]); 
+class collections extends Contract {
+   
+  async queryProduct(ctx, collectionName,productId) {
+    let productAsBytes = await ctx.stub.getPrivateData(collectionName,productId); 
     if (!productAsBytes || productAsBytes.toString().length <= 0) {
       throw new Error('Product does not exist: ');
     }
-    console.log(productAsBytes.toString());
-    return productAsBytes;
+    let product =  JSON.parse(productAsBytes);
+    return product;
   }
 
   
 
-  async addProduct(stub, args) {
+  async addProduct(ctx, collectionName, productId,name,price,seller) {
     console.info('============= START : Adding Product===========');
-    if (args.length != 5) {
-      throw new Error('Incorrect number of arguments. Expecting 5');
-    }
 
-    var product = {
-      docType: 'products',
-      productId: args[1],
-      name: args[2],
-      price: args[3],
-      seller: args[4]
+   let product = {
+      ProductId: productId,
+      Name: name,
+      Price: price,
+      Seller: seller
     };
 
-    await stub.putPrivateData(args[0],args[1], Buffer.from(JSON.stringify(product)));
+    await ctx.stub.putPrivateData(collectionName,productId Buffer.from(JSON.stringify(product)));
     console.info('============= END : Private Product Added===========');
+    return "Product Added.."
   }
 };
 
-shim.start(new Chaincode());
+module.exports = collections;
